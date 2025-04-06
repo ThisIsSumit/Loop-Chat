6677a22ea74941caecaa1efd265609b9f3d9d83e
ThisIsSumit <sumitkumar453827@gmail.com>
2025-04-06 07:56:13 +0530

    app is connected to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:loop_talk/pages/chat_page.dart';
 import 'package:loop_talk/pages/home.dart';
 import 'package:loop_talk/pages/onboarding.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp( );
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+              apiKey: "AIzaSyCV712201E07ogL38eiSXR-YVtWuVxPPDE",
+              appId: "1:1096809484823:android:f5a24d4810e90f1f13c252",
+              messagingSenderId: '1096809484823',
+              projectId: 'loop-talk-b7570'),
+        )
+      : await Firebase.initializeApp();
+
   runApp(const LoopTalk());
 }
 
@@ -19,6 +34,6 @@ class LoopTalk extends StatelessWidget {
               seedColor: Colors.deepPurple,
             ),
             useMaterial3: true),
-        home: ChatPage());
+        home: Onboarding());
   }
 }
